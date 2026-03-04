384a6704593d39745d83403dfbb0bf2227b94554
Gustavo-Felix <gustavofelix2007@gmail.com>
2026-03-04 17:58:20 -0300

    feat: implement main method to demonstrate Employee and CheckingAccount usage

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import entity.CheckingAccount;
+import entity.Employee;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import java.time.Instant;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee(1L, "John Doe", Instant.parse("2020-01-01T00:00:00Z"));
+        CheckingAccount checkingAccount = new CheckingAccount(1L, "John Doe", 1000.0);
+
+        System.out.println("Employee 01 - : " + employee);
+        System.out.println("Checking Account 01 - : " + checkingAccount);
+
+        System.out.println(employee.TempoTrabalho());
+
+    }
 }
